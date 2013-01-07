6b6d8f9eaceffd6d9a105c7e319a346f77138397
Caparico <kobi.birdman@gmail.com>
2013-01-07 23:04:34 +0200

    add script.js file and test jQuery

--- index.php
+++ index.php
@@ -5,10 +5,11 @@
 		<title>Foto Cards</title>
 		<link rel="stylesheet" href="css/fotocards.min.css">
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+			<script type="text/javascript" src="scripts/script.js"></script>
+
 	</head>
 	<body>
 		<h1>Foto Cards</h1>
 		<h2>Your Online Photo Editor of Choice</h2>
 	</body>
-
 </html>
